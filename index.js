bb3474cb782bfafa5420b8849f5478c242b821ed
Ransom Sunday <sundayyoung64@gmail.com>
2023-06-14 22:10:50 +0100

    added code for dom

---
+++ index.js
@@ -0,0 +1,10 @@
+// Dom
+document.body.style.backgroundColor = 'fuchsia';
+// 
+// let button = document.getElementById("changeBackground");
+
+// button.addEventListener("click", () => {
+//     document.body.style.backgroundColor = 'fuchsia';
+// });
+
+
---
+++ node.js
@@ -0,0 +1,21 @@
+// let demoId = document.getElementById('demo');
+// demoId.style.border="1px solid red";
+
+// let demoClass = document.getElementsByClassName('demo');
+// console.log(demoClass);
+// for(let i =  0; i < demoClass.length; i++){
+//     demoClass[0].style.border="2px solid green";
+// }
+
+// let demoTag = document.getElementsByTagName('article');
+// for(let i = 1; i < demoTag.length; i++){
+//     demoTag[i].style.fontSize = "50px";
+// }
+
+// let QuerySelector = document.querySelector('#demo-query');
+// QuerySelector.style.color= 'red';
+// let QueryAll = document.querySelectorAll('.demo-query-all');
+// for(let i = 0; i < QueryAll.length; i++){
+//     QueryAll[i].style.border="5px solid pink";
+// }
+
